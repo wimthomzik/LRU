8bc049472b1b330f3fbfdb38f3d5513fc3a9f3a8
wimthomzik <82528324+wimthomzik@users.noreply.github.com>
2023-10-10 08:28:32 +0000

    define LRU Cache class with a queue as list, a cache as dic and max. capacity; implement refer function and display function for queue and cache; initite a cache; test cache functions

--- lru.py
+++ lru.py
@@ -1,11 +1,44 @@
 #Create Class LRUCache
-    #Declare List type int
-    #Declare unordered map of type <int, list<int>>
-    #Declare capacity cache
+class LRUCache():
 
-    #Create refer function
-        #if value is not in queue push in front and delete last 
-        #if value present push it in front
+    def __init__(self, size):
+        self.queue = [] #queue as list
+        self.cache = {} #cache as dic
+        self.capacity = size #capacity Cache
+
+    #refer function
+    def refer(self, input):
+        #input is not in cache
+        if input not in self.cache.keys():
+            #queue is full
+            if len(self.queue) == self.capacity:
+                #remove item from cache and queue, that is last in queue
+                self.cache.pop(self.queue[-1])
+                self.queue.pop()
+        #input is in cache
+        else:
+            #remove input from queue
+            self.queue.remove(input)
+
+        #add input to cache and queue in front
+        self.queue.insert(0, input)
+        self.map[input] = 0
+
     
-    #Create display function
-        #print lru cache starting from the front
+    #display function
+    def display(self):
+        print(self.queue)
+        print(self.map)
+
+#initiate cache
+lru_cache = LRUCache(4)
+
+#test
+lru_cache.refer(3)
+lru_cache.refer(2)
+lru_cache.refer(4)
+lru_cache.refer(5)
+lru_cache.refer(6)
+lru_cache.refer(6)
+lru_cache.refer(6)
+lru_cache.display()
